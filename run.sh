9545c1db7c82f306c6d73b8d2bb2551687f48d7d
Luis Diaz <dev.brainstack@gmail.com>
2025-10-05 17:34:26 +0000

    armar comando python

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
 CARPETA=$1
+BIN_PYTHON=$2
+SCRIPT_PYTHON=$3
+CARPETA_DESTINO=$4
 
-find "$CARPETA" -type f -name "*.xlsx" -print
+mkdir -p $CARPETA_DESTINO
+
+find "$CARPETA" -type f -name "*.xlsx" | while IFS= read .r archivo; do
+    
+    NOMBRE_ARCHIVO=$(basename "$archivo")
+
+    echo "$BIN_PYTHON $SCRIPT_PYTHON $archivo $CARPETA_DESTINO/$NOMBRE_ARCHIVO"
+
+done
