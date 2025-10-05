124f774b15da64ee152c45f539f2d10af9e0d84a
Luis Diaz <dev.brainstack@gmail.com>
2025-10-05 19:26:24 +0000

    crear carpeta con el year

--- scripts/xlsx_date.py
+++ scripts/xlsx_date.py
@@ -6,6 +6,8 @@ import zipfile
 import xml.etree.ElementTree as ET
 from pathlib import Path
 import sys
+import shutil
+from datetime import datetime
 
 def get_xlsx_dates(archivo):
     """
@@ -50,8 +52,51 @@ def get_xlsx_dates(archivo):
                     'error' : str(e)
                 }
 
+def get_year(metadata, dato="created"):
+    """
+    Funcion que retorna el year
+    """
+    data_date = metadata.get(dato)
+
+    if data_date:
+        try:
+            dt = datetime.fromisoformat(data_date.replace('Z', '+00:00'))
+            return dt.year
+        except ValueError:
+            return None
+        
+        return None
+
+def copy_to_year_folder(file_path, year, base_dir):
+    """
+    Funcion que copia el archivo xlsx a su respondiente carpeta
+    """
+    if year is None:
+        year_folder = Path(base_dir) / "unknow_year"
+    else:
+        year_folder = Path(base_dir) / str(year)
+
+    year_folder.mkdir(parents=True, exist_ok=True)
+
+    file_path = Path(file_path)
+    dest_path = year_folder / file_path.name
+    shutil.copy2(file_path, dest_path)
+
+    return dest_path
+
+
 if __name__ == "__main__":
     op = sys.argv[1:]
-    respuesta = get_xlsx_dates(op[0])
+    
+    PATH_ARCHIVO = op[0]
+    CARPETA_RAIZ = op[1]
 
-    print(f"el archivo {op[0]} tiene {respuesta}")
+    respuesta = get_xlsx_dates(PATH_ARCHIVO)
+    year = get_year(respuesta)
+
+    destino = copy_to_year_folder(
+        PATH_ARCHIVO,
+        year,
+        CARPETA_RAIZ)
+
+    print(f"el archivo {PATH_ARCHIVO} Se guarda en {year}")
