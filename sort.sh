61562bc2ed4547da08f027f6bf945d4ca87a2701
Luis Diaz <dev.brainstack@gmail.com>
2025-10-05 19:30:29 +0000

    crear script para hacer sort por year

---
+++ sort.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+CARPETA=$1
+BIN_PYTHON=$2
+SCRIPT_PYTHON=$3
+CARPETA_DESTINO=$4
+
+mkdir -p $CARPETA_DESTINO
+
+find "$CARPETA" -type f -name "*.xlsx" | while IFS= read -r archivo; do
+    
+    NOMBRE_ARCHIVO=$(basename "$archivo")
+
+    echo "$BIN_PYTHON $SCRIPT_PYTHON $archivo $CARPETA_DESTINO"
+
+done
